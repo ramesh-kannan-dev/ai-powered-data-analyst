69d251494a3c5dd0a7fb97c30a0509147ae5442f
ramesh-kannan-dev <rameshlisa7@gmail.com>
2026-04-18 11:19:29 +0530

    Delete backend directory

--- backend/routes.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from .endpoints import dataset, analysis, report, history
-
-api_router = APIRouter()
-api_router.include_router(dataset.router, prefix="/dataset", tags=["dataset"])
-api_router.include_router(analysis.router, prefix="/analysis", tags=["analysis"])
-api_router.include_router(report.router, prefix="/report", tags=["report"])
-api_router.include_router(history.router, prefix="/history", tags=["history"])
